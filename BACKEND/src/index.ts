650228730d56df43bf88e98b77f167a2753a4dc9
John Patrick <jpattrick538@gmail.com>
2023-08-20 16:55:59 +0300

    feat:allow cors credentials

--- BACKEND/src/index.ts
+++ BACKEND/src/index.ts
@@ -1,15 +1,26 @@
+
 import  express  from "express";
 import cors from "cors"
-import { router } from "./routes/auth";
+import { connection } from "../config/database";
+import { router } from "./routes/routes";
+import cookieParser from "cookie-parser"
+import { exit } from "process";
+
+
+if(!connection.initialize())exit();
 
 const app = express();
-const PORT:number | string = process.env.PORT || 3000;
+const PORT:number | string = process.env.VITE_PORT || 3000 ;
 
 app.use(express.json());
+app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:3000"
+    origin:"http://localhost:3000",
+    credentials:true
 }));
+
 router(app);
+
 app.listen(PORT ,():void=>{
 console.log(`Server running on port: ${PORT}`)
 })
