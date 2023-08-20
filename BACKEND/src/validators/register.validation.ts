fcf35bc4348bc43e0b79da2414f312963bb67be6
John Patrick <jpattrick538@gmail.com>
2023-08-20 16:56:43 +0300

    feat:add upate passpwrd validator

--- BACKEND/src/validators/register.validation.ts
+++ BACKEND/src/validators/register.validation.ts
@@ -9,3 +9,15 @@ export const RegisterValidation = Joi.object(
         password_confirm:Joi.string().required(),
     }
 )
+export const loginValidation = Joi.object(
+    {
+        email:Joi.string().email().required(),
+        password:Joi.string().required(),
+    }
+)
+export const passwordValidation = Joi.object(
+    {
+        oldPassword:Joi.string().required(),
+        newPassword:Joi.string().required()
+    }
+)
