c8627b417ce79300bbc0706990822ed118f9f578
John Patrick <jpattrick538@gmail.com>
2023-08-31 23:14:21 +0300

    feat:add auth routes

--- BACKEND/src/routes/routes.ts
+++ BACKEND/src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authenticatedUser, login, logoutUser, register, updateInfo, updatePassword } from "../controllers/authcontroller";
 import { authMiddlware } from "../middlewares/auth.middleware";
+import { createUsers, deleteUsers, fetchAllUsers, fetchOneUsers, updateUsers } from "../controllers/usercontroller";
 
 export const router =(router:Router)=>{
     router.post("/api/register",register)
@@ -9,4 +10,10 @@ export const router =(router:Router)=>{
     router.post("/api/logout",authMiddlware,logoutUser)
     router.put("/api/user/info",authMiddlware,updateInfo)
     router.patch("/api/user/info",authMiddlware,updatePassword)
+    
+    router.get("/api/users",authMiddlware,fetchAllUsers)
+    router.post("/api/users",authMiddlware,createUsers)
+    router.get("/api/users/:id",authMiddlware,fetchOneUsers)
+    router.put("/api/users/:id",authMiddlware,updateUsers)
+    router.delete("/api/users/:id",authMiddlware,deleteUsers)
 } ;
