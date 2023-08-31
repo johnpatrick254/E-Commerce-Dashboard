b781f9c1ae14692e872054745cb40283036116de
John Patrick <jpattrick538@gmail.com>
2023-08-31 23:13:39 +0300

    feat:add auth routes

--- BACKEND/src/routes/auth.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { register } from "../controllers/authcontroller";
-
-export const router =(router:Router)=>{
-    router.post("/api/register",register)
-} ;
