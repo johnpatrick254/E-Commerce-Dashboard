e954c9ccf5bdb87173345e5488a167fe55444cee
John Patrick <jpattrick538@gmail.com>
2023-09-02 19:50:04 +0300

    feat:create orders entity

---
+++ BACKEND/src/entities/orders.entity.ts
@@ -0,0 +1,32 @@
+import { PrimaryGeneratedColumn,Column, CreateDateColumn, OneToMany, Entity } from "typeorm"
+import { OrderItem } from "./orderitem.entity";
+
+@Entity()
+export class Orders{
+    @PrimaryGeneratedColumn()
+    id:number;
+    
+    @Column()
+    first_name:string;
+
+    @Column()
+    last_name:string;
+
+    @Column()
+    email:string;
+
+    @CreateDateColumn()
+    created_at:string;
+
+    @OneToMany(()=>OrderItem,OrderItem=>OrderItem.order)
+    order_items:OrderItem[]
+
+
+    get name ():string{
+        return this.email + " " + this.last_name
+    }
+
+    get totals():number{
+        return this.order_items.reduce((a,b)=>a + (b.price * b.quantity),0)
+    }
+}
