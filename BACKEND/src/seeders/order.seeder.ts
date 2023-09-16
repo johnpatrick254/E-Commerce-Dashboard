060305e5bf0be10fcb80d2cb7320d76d6860c90a
John Patrick <jpattrick538@gmail.com>
2023-09-16 22:38:23 +0300

    feat:increaded seed data size

--- BACKEND/src/seeders/order.seeder.ts
+++ BACKEND/src/seeders/order.seeder.ts
@@ -1,30 +1,34 @@
-import { connection } from "../../config/ormconfig"
-import {fakerEN} from "@faker-js/faker"
-import { Orders } from "../entities/orders.entity"
-import { randomInt } from "crypto"
-import { OrderItem } from "../entities/orderitem.entity"
-import { log } from "console"
+import { connection } from "../../config/ormconfig";
+import { fakerEN } from "@faker-js/faker";
+import { Orders } from "../entities/orders.entity";
+import { randomInt } from "crypto";
+import { OrderItem } from "../entities/orderitem.entity";
+import { Product } from "../entities/product.entity";
 
-export const seedOrders = async()=>{
-const orderRepo = connection.getRepository(Orders)
-const orderItemRepo= connection.getRepository(OrderItem)
-for (let index = 0; index < 70 ; index++) {
+export const seedOrders = async () => {
+  const orderRepo = connection.getRepository(Orders);
+  const orderItemRepo = connection.getRepository(OrderItem);
+  const productItemRepo = connection.getRepository(Product);
+
+  for (let index = 0; index < 70; index++) {
     const order = await orderRepo.save({
-        first_name:fakerEN.person.firstName(),
-        last_name:fakerEN.person.lastName(),
-        email:fakerEN.internet.email(),
-        created_at:fakerEN.date.past({years:2}).toISOString()
-        })
-    for(let i = 0 ;i<randomInt(1,6);i++){
-     const oi= await orderItemRepo.save({
-        productTitle:fakerEN.commerce.productName(),
-        price:randomInt(20,850),
-        quantity:randomInt(1,15),
-        order:order,
-      })
-      console.log(oi);
-      
+      first_name: fakerEN.person.firstName(),
+      last_name: fakerEN.person.lastName(),
+      email: fakerEN.internet.email(),
+      created_at: fakerEN.date.past({ years: 2 }).toISOString(),
+    });
+
+    for (let i = 0; i < randomInt(1, 6); i++) {
+      const io = new OrderItem()
+      io.productTitle= fakerEN.commerce.productName();
+      io.quantity=randomInt(5, 9);
+      io.order= order;
+      io.product_id= randomInt(1, 50);
+      await io.setPrice(productItemRepo);
+      await orderItemRepo.save(io);
+      console.log(io);
     }
-}
-process.exit(0)
-}
+  }
+
+  process.exit(0);
+};
--- BACKEND/src/seeders/product.seeder.ts
+++ BACKEND/src/seeders/product.seeder.ts
@@ -4,12 +4,14 @@ import {fakerEN} from "@faker-js/faker"
 
 export const seedProducts = async()=>{
 const productRepo = connection.getRepository(Product)
-for (let index = 0; index < 30 ; index++) {
+for (let index = 0; index < 50 ; index++) {
+    const original_price = fakerEN.commerce.price()
     await productRepo.save({
         name:fakerEN.commerce.productName(),
         description:fakerEN.commerce.productDescription(),
-        price:+fakerEN.commerce.price(),
-        image:fakerEN.image.urlLoremFlickr({category:'commerce'})
+        price:Math.floor(+original_price + ((+original_price * 20)/100)),
+        original_price:+original_price,
+        image:fakerEN.image.urlLoremFlickr({category:'tech'})
     })
     
 }
