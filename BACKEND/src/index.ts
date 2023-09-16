59867fbbbdc7e8fb4ba942b6178f3e664ebda22d
John Patrick <jpattrick538@gmail.com>
2023-09-16 22:40:50 +0300

    feat:add vite localhost to cors origin

--- BACKEND/src/index.ts
+++ BACKEND/src/index.ts
@@ -15,7 +15,7 @@ const PORT:number | string = process.env.VITE_PORT || 3000 ;
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"http://localhost:5173",
     credentials:true
 }));
 
