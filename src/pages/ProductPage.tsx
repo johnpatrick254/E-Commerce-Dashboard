64d927ef43d537e98aee114c55572038087c4e1b
John Patrick <jpattrick538@gmail.com>
2023-09-18 22:41:29 +0300

    feat: dynamically render form data when updating

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,45 +1,54 @@
 import { useParams } from "react-router-dom"
 import { Nav } from "../components/nav/Nav"
 import "../styles/productpage/productpage.style.css"
-import { useEffect, useState } from "react"
-import { ProductData } from "../Utils/ProductPage/ProductData"
-import { fetchProductDetail } from "../Utils/ProductPage/fetchProductDetail"
+import { useDeleteProductMutation, useGetProductByIDQuery } from "../api/Products.slice"
+import { useState } from "react"
+import { ProductForm } from "../components/Products/ProductForm"
 
-export const ProductPage:React.FC=()=>{
-    const {id}  = useParams()
-    const [productData, setProductData] = useState<ProductData | null>(null)
-    useEffect(()=>{
-     const fetchproductData = async (id:number)=>{
-         const data = await fetchProductDetail(id)
-         setProductData(data)
-     }
-    fetchproductData(+id!)
-    },[])
-    return<>
-     <Nav/>
-     <div className="product-container">
-        <div className="product-details_main">
-            <div className="title">
-                <h1>{productData && productData.name}</h1>
-                <button onClick={()=>{window.history.back()}} >Back</button>
+export const ProductPage: React.FC = () => {
+    const { id } = useParams()
+    const [deleteProduct, _isLoading] = useDeleteProductMutation();
+    const { data: productData } = useGetProductByIDQuery(id!)
+    const [displayForm, setDisplayForm] = useState(false)
+
+    return <>
+        <Nav />
+        <div className="product-container">
+            <div className="product-details_main">
+                <div className="title">
+                    <h1>{productData && productData.name}</h1>
+                    <button onClick={() => { window.history.back() }} >Back</button>
+                </div>
+                <h2>Price: ${productData && productData.price}</h2>
+                <p><strong>Category:</strong> {productData && productData.category}</p>
+                <div className="product-buttons">
+                    <button onClick={() => { setDisplayForm(!displayForm) }} >
+                        Edit Product</button>
+                    <button onClick={() => {
+                        deleteProduct(id!);
+                        window.history.back()
+                    }}>Delete Product</button>
+                </div>
             </div>
-
-            <h2>Price: ${productData && productData.price}</h2>
-            <p><strong>Category:</strong> {productData && productData.category}</p>
-            <div className="product-buttons">
-            <button>Delete Product</button>
-            <button>Edit Product</button>
+            <div className="product-details_image">
+                <img src={productData ? productData.image : ''} alt="product image" />
+            </div>
+            <div className="product-details_secondary">
+                <h3>Description</h3>
+                <p>
+                    {productData && productData.description}
+                </p>
             </div>
         </div>
-        <div className="product-details_image">
-            <img src={productData ? productData.image:''} alt="product image" />
-        </div>
-        <div className="product-details_secondary">
-            <h3>Description</h3>
-            <p>
-                {productData && productData.description}
-            </p>
-        </div>        
-     </div>
+        <ProductForm
+            display={displayForm}
+            name={productData?.name}
+            category={productData?.category}
+            price={productData?.price}
+            description={productData?.description}
+            setDisplayForm={setDisplayForm}
+        />
+
+
     </>
 }
