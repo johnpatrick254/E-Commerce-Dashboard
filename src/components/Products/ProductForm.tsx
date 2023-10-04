b402a97e0bd769e35f493517cf6c49f9084ad163
John Patrick <jpattrick538@gmail.com>
2023-10-04 19:59:10 +0300

    feat:make product form dynamic

--- src/components/Products/ProductForm.tsx
+++ src/components/Products/ProductForm.tsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
-import { useUpdateProductMutation } from "../../api/Products.slice";
+import { useCreateProductMutation, useUpdateProductMutation } from "../../api/Products.slice";
+import { toast } from 'react-toastify';
+import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
 
 type FormData = {
-  display: boolean;
+  id?:string;
+  display?: boolean;
   name?: string;
   description?: string;
   price?: number;
   category?: string;
   image?: File | string;
-  setDisplayForm:(p:boolean)=>void
+  setDisplayForm: (p: boolean) => void
 };
 
 export const ProductForm: React.FC<FormData> = ({
@@ -18,11 +22,14 @@ export const ProductForm: React.FC<FormData> = ({
   description,
   price,
   category,
+  id
 }) => {
 
-  const [updateProduct, _isLoading] = useUpdateProductMutation()
+  const [updateProduct, _isLoading] = useUpdateProductMutation();
+  const[createProduct]= useCreateProductMutation()
 
-  const initialFormData = {
+  let initialFormData:any = {
+    id:id,
     name: name,
     description: description,
     price: price,
@@ -30,9 +37,11 @@ export const ProductForm: React.FC<FormData> = ({
   };
 
   const [formData, setFormData] = useState<any>(initialFormData);
-useEffect(()=>{
-setFormData(initialFormData)
-},[display])
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setFormData(initialFormData)
+  }, [display])
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setFormData({
@@ -40,7 +49,7 @@ setFormData(initialFormData)
       [name]: value,
     });
   };
-  
+
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -51,14 +60,61 @@ setFormData(initialFormData)
       });
     }
 
-  }; 
+  };
 
-  const handleCancelForm =()=>{
+  const handleCancelForm = () => {
     setDisplayForm(!display);
     setFormData(initialFormData);
   }
-  const handleSubmit = () => {
-    updateProduct({ body: formData })
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    let updates;
+   
+    if (formData.image) {
+      const imageForm = new FormData()
+      imageForm.append("image",formData.image)   
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:3000/api/upload',
+        data: imageForm,
+        withCredentials: true
+      };
+      const imageUrl: Promise<string | null> = await axios.request(config)
+        .then((response) => {
+          return response.data.url;
+        })
+        .catch((error) => {
+          console.log(error);
+          return null;
+        });
+      if (imageUrl !== null && imageUrl !== undefined) {
+        const { id, ...data } = formData     
+        updates = { ...data, ['image']: imageUrl }
+      }else{
+        toast("Image Upload Failed");
+        setLoading(false)
+        return;
+      }
+
+    } else {
+      const { image,id, ...data } = formData
+      updates = data
+      
+    }
+    if(id){
+      updateProduct({ id:id,body: updates });
+    }else{
+       const original_price = formData.price -((formData.price * 20) / 100 )
+       const body ={
+        ...updates,
+        ['original_price']: original_price,
+      }
+      createProduct({body:body})
+    }
+    setDisplayForm(!display);
+    setLoading(false)
+
   }
   return (
     <>
@@ -68,63 +124,63 @@ setFormData(initialFormData)
             <button className="cancel" onClick={handleCancelForm}>Cancel</button>
             <div className="header">
               <div>
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                className="product-name"
-                value={formData.name}
-                onChange={handleInputChange}
-              />
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="product-name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                />
               </div>
               <div>
-              <label htmlFor="category">Category</label>
-              <input
-                type="text"
-                name="category"
-                id="category"
-                value={formData.category}
-                onChange={handleInputChange}
-              />
+                <label htmlFor="category">Category</label>
+                <input
+                  type="text"
+                  name="category"
+                  id="category"
+                  value={formData.category}
+                  onChange={handleInputChange}
+                />
               </div>
             </div>
             <div className="textarea">
-            <label htmlFor="description">Description</label>
-            <textarea
-              name="description"
-              cols={30}
-              rows={10}
-              value={formData.description}
-              id="description"
-              onChange={handleInputChange}
-              placeholder="Enter Description"
-            >
-            </textarea>
+              <label htmlFor="description">Description</label>
+              <textarea
+                name="description"
+                cols={30}
+                rows={10}
+                value={formData.description}
+                id="description"
+                onChange={handleInputChange}
+                placeholder="Enter Description"
+              >
+              </textarea>
             </div>
             <div className="footer">
               <div>
-              <label htmlFor="price">Price</label>
-              <input
-                type="text"
-                name="price"
-                id="price"
-                value={formData.price}
-                onChange={handleInputChange}
-              />
+                <label htmlFor="price">Price</label>
+                <input
+                  type="text"
+                  name="price"
+                  id="price"
+                  value={formData.price}
+                  onChange={handleInputChange}
+                />
               </div>
               <div>
-              <label htmlFor="image">Image</label>
-              <input
-                type="file"
-                name="image"
-                id="image"
-                accept="image/*"
-                onChange={handleImageChange}
-              />
+                <label htmlFor="image">Image</label>
+                <input
+                  type="file"
+                  name="image"
+                  id="image"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                />
               </div>
             </div>
-            <button type="submit" onClick={handleSubmit}>Submit</button>
+           { loading ? <ClipLoader className="spinner" color="#000000" size={"1.85rem"} />: <button type="submit" onClick={e=>{e.preventDefault();setLoading(true);handleSubmit(e as any)}}>Submit</button>}
           </form>
         </div>
       )}
---
+++ src/components/Products/ProductPageCategory.tsx
@@ -0,0 +1,27 @@
+import { useGetAllProductsQuery } from "../../api/Products.slice"
+import { ProductCard } from "./ProductCard"
+
+export const ProductPageCategory: React.FC<{ category: string }> = ({ category }) => {
+
+    const { data: productData, error, isLoading } = useGetAllProductsQuery({ page: "1", category: category })
+
+    return <>
+        <div className="product-category">
+            <h2>{category}</h2>
+            <div className="products">
+                {
+                    productData?.data.map(product => {
+                        return <ProductCard
+                           id={product.id}
+                            key={product.id}
+                            name={product.name}
+                            price={product.price}
+                            image={product.image}
+                        />
+                    })
+                }
+            </div>
+
+        </div>
+    </>
+}
