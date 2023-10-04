c9abc01bf25c4387271a8bb1d8b158fa67854e21
John Patrick <jpattrick538@gmail.com>
2023-10-04 20:06:01 +0300

    feat:add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import { AuthenicatedUser } from "./Utils/util";
-import Login from "./components/Login/Login";
+import Login from "./pages/Login";
 import { UserProvider } from "./contexts/useContexts";
 import { Home } from "./pages/Home"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
+import { ProductPage } from "./pages/ProductPage";
+import { ProductsPage } from "./pages/ProductsPage";
+import { UsersPage } from "./pages/User";
+import { OrdersPage } from "./pages/Orders";
 
 const fetchAuthenticatedUser = async (setUser: Function) => {
   const user = await axios.get("http://localhost:3000/api/user", {
@@ -44,6 +48,10 @@ function App() {
           <Routes>
             <Route path="/" Component={Home} />
             <Route path="/login" Component={Login} />
+            <Route path="/products/:id" Component={ProductPage} />
+            <Route path="/products" Component={ProductsPage} />
+            <Route path="/user" Component={UsersPage} />
+            <Route path="/orders" Component={OrdersPage} />
           </Routes>
         </Router>
       </UserProvider.Provider>
